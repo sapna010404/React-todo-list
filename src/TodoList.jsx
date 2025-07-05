070980dfa9cf8ab92c32ac22ede01d195989a992
Sapna <sapna010404@gmail.com>
2025-07-06 00:23:28 +0530

    mark done functionality added

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,16 +1,19 @@
 import {useState} from "react";
 import {v4 as uuidv4} from "uuid";
 
+
 export default function TodoList(){
     let [todo, setTodo]=useState([]);
     let [newTask, setTask]=useState("");
+
+
 
     function addItem(){
         setTask("");
 
         if(newTask!=""){
             setTodo((todo)=>{
-                return [...todo,{task:newTask , id: uuidv4()}];
+                return [...todo,{task:newTask , id: uuidv4(), isDone:false}];
             });
         }
         }
@@ -21,12 +24,23 @@ export default function TodoList(){
     }
 
     function removeItem(id){
-        setTodo((todoList)=>(todoList.filter((item)=>( item.id!=id))));
+        setTodo((prevTodo)=>(prevTodo.filter((item)=>( item.id!=id))));
+    }
+
+    function markDone(id){
+        setTodo((prevTodo)=>(prevTodo.map((item)=>
+            item.id==id? {...item, isDone: !item.isDone} : item
+        )))
+    }
+
+    function markAllDone(){
+        setTodo((prevTodo)=>(prevTodo.map((item)=>({...item,isDone:true}))));
     }
 
     return (
         <>
         <input type="input" placeholder="add a task..." id="Input" style={{height: "30px",}} onChange={updateTask} value={newTask}></input>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
         <button onClick={addItem}>Add Task</button>
 
         <br></br> <br></br><br></br>
@@ -35,12 +49,18 @@ export default function TodoList(){
         <ul>
             {todo.map((item)=>{
             return <li key={item.id}>
-                <span>{item.task} &nbsp;&nbsp;&nbsp;</span> 
+                {item.isDone?
+                    <span style={{textDecoration:"line-through"}}>{item.task}</span> :
+                    <span>{item.task}</span>
+                }
+                &nbsp;&nbsp;&nbsp;&nbsp;
                 <button onClick={()=>{removeItem(item.id)}}>Delete</button>
-                </li> 
-            
+                &nbsp;&nbsp;&nbsp;&nbsp;
+                <button onClick={()=>markDone(item.id)}> {item.isDone? "Undo": "Mark as Done"}</button>
+                </li>
             })}
         </ul>
+        <button onClick={()=>markAllDone()}>Mark all done</button>
         </>
     )
 }
